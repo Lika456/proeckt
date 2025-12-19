8a67197c70f687656eac7e8107ad9a2914f0bb0d
Lika456 <a_godovalova07@mail.ru>
2025-12-19 16:08:40 +0400

    Update app.py

--- app.py
+++ app.py
@@ -463,4 +463,5 @@ if __name__ == '__main__':
     if not os.path.exists("users.txt"):
         with open("users.txt", "w", encoding="utf-8") as f:
             f.write("user:user1::::\nadmin:admin1::::\n")
-    app.run(debug=True)
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
